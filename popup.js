e762df6b752323605dde0980da6583a16ea7864b
Matt Bierner <mattbierner@gmail.com>
2015-07-26 13:54:53 -0700

    Styling popup buttons.

--- popup.js
+++ popup.js
@@ -1,14 +1,26 @@
-$(function(){
-
-
-$('#toggle').click(function(){
+/**
+    Toggle the content script on or off.
+*/
+var toggleContentScript = function() {
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
         chrome.tabs.sendMessage(
             tabs[0].id,
             {method: "toggle"},
             function() { });
     });
-});
+};
+
+/**
+    Toggle the effect on or off.
+*/
+var toggle = function() {
+    toggleContentScript();
+};
+
+
+$(function(){
+
+$('#toggle').click(toggle);
 
 $('#options').click(function(){
     chrome.runtime.openOptionsPage();
